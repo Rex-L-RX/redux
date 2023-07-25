6e5f0850a3c56b7b45918ac2e407c622731d1bdc
Runxiao-Liu <liurunxiao233@gmail.com>
2023-07-25 21:16:18 +1000

    Rdux v5.2 add Provider

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
 import Count from './containers/count'
-import store from './redux/store'
 
 function App() {
   return (
     <div className="App">
-      <Count store={store}/>
+      <Count/>
     </div>
   );
 }
