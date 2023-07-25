287e16088111d474d0fcc528733081435232adb7
Runxiao-Liu <liurunxiao233@gmail.com>
2023-07-25 22:07:00 +1000

    Redux v5.3 integrate container and UI

--- src/containers/count/index.jsx
+++ src/containers/count/index.jsx
@@ -1,13 +1,54 @@
-import CountUI from '../../components/Count'
 import {
     createIncrementAction,
     createDecrementAction,
     createAsyncAction
 } from '../../redux/count_action'
 import { connect } from 'react-redux'
-
+import React, { Component } from 'react'
 function mapStateToProps(state) {
     return { count: state }
+}
+
+
+
+class Count extends Component {
+
+    increment = () => {
+        const {value} = this.selectedNumber
+        this.props.increment(value*1)
+    }
+    decrement = () => {
+        const {value} = this.selectedNumber
+        this.props.decrement(value*1)
+    }
+    incrementIfOdd = () => {
+        const {value} = this.selectedNumber
+        if(this.props.count % 2 !== 0){
+            this.props.increment(value*1)
+        }
+    }
+    incrementAsyc = () => {
+        const {value} = this.selectedNumber
+        this.props.incrementAsync(value*1,500)
+    }
+    render() {
+        return (
+            <div>
+                <h1>Sum: {this.props.count}</h1>
+                <select ref={c => this.selectedNumber = c}>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                </select>&nbsp;
+                <button onClick={this.increment}>+</button>&nbsp;
+                <button onClick={this.decrement}>-</button>&nbsp;
+                <button onClick={this.incrementIfOdd}>add if sum is odd</button>&nbsp;
+                <button onClick={this.incrementAsyc}>Asyc add</button>
+            </div>
+
+        )
+    }
 }
 
 //mapDispatchToProps can be a function or it can be replaced by an obj
@@ -30,4 +71,5 @@ const mapDispatchToProps = {
     decrement: createDecrementAction,
     incrementAsync: createAsyncAction
 }
-export default connect(mapStateToProps, mapDispatchToProps)(CountUI)
+export default connect(mapStateToProps, mapDispatchToProps)(Count)
+ 
